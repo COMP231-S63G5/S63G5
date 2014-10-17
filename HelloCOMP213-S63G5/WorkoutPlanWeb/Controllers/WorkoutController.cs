4a46c10cc674ce5ebefe2aba648bc8ede540cd9c
AO-COMP231S63G5 <azim.centennial@gmail.com>
2014-10-16 18:04:38 -0700

    Layout updated. Home page was updated to include link to new workout. Added a new workout controller with new view.

---
+++ HelloCOMP213-S63G5/WorkoutPlanWeb/Controllers/WorkoutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkoutPlanWeb.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewPlan()
+        {
+            return View();
+        }
+
+    }
+}
