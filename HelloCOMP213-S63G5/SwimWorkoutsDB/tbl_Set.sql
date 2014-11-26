cbc9f44d5c638ff1b9fad49440e3bea6b106a167
patelh18 <patelh18@gmail.com>
2014-11-26 15:25:35 -0500

    changed db structure

--- HelloCOMP213-S63G5/SwimWorkoutsDB/tbl_Set.sql
+++ HelloCOMP213-S63G5/SwimWorkoutsDB/tbl_Set.sql
@@ -2,7 +2,7 @@
 (
 	[setID] INT IDENTITY(1000,1) NOT NULL PRIMARY KEY,
 	[setType] NVARCHAR(50) not null,
-	[planID] int not null CONSTRAINT "foreignkeyPlanid" foreign key references tbl_workoutplan([planID]) on delete cascade,
+	[planID] int not null CONSTRAINT "foreignkeyPlanid" FOREIGN KEY REFERENCES tbl_WorkoutPlan([planID]) ON DELETE CASCADE,
 	[repeats] int null,
 	[stroke] varchar(10) null,
 	[pace] varchar(10),
@@ -13,5 +13,5 @@
 	[energyName] VARCHAR(5) NOT NULL ,
 	[totalDistance] int NOT NULL DEFAULT 0,
 	[orderID] int NOT NULL ,
-	[parentID] int NOT NULL CONSTRAINT "foreignkeySetid" foreign key references tbl_Set([setID]) on delete cascade,
+	[parentID] int NOT NULL CONSTRAINT "foreignkeySetid" FOREIGN KEY REFERENCES tbl_Set([setID])
 )
