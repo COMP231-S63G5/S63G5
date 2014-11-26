112406ae5ad0d419b6a70dbe9eab5edc878ebde6
patelh18 <patelh18@gmail.com>
2014-11-26 01:28:41 -0500

    new database structure according to new design

--- HelloCOMP213-S63G5/DBWorkout/tbl_stroke.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[tbl_stroke]
-(
-	[ID] INT IDENTITY(100,1) NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NOT NULL, 
-    [Description] VARCHAR(250) NULL
-)
-
--- HelloCOMP213-S63G5/DBWorkout/tbl_workoutplan.sql
+++ HelloCOMP213-S63G5/DBWorkout/tbl_workoutplan.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[tbl_workoutplan]
 (
-	ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	planDate date not null
+	[planID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	planDate date not null, 
+    [totalDistance] INT NULL, 
+    [totalDuration] NCHAR(10) NULL
 );
 
